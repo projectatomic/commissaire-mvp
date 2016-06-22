cbac5984a33922f85729e6c4b01d1b36b400e1b8
Steve Milner <smilner@redhat.com>
2016-06-22 14:05:39 -0400

    Added base class for the upcoming store system.
    This is some foundation work for upcoming cpd-126 related changes.
    
    See #126.
    
    Closes: #153
    Approved by: mbarnes

---
+++ test/test_store_handler_base_class.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2016  Red Hat, Inc
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Test cases for the commissaire.store.StoreHandlerBase class.
+"""
+
+from . import TestCase
+from commissaire.store import StoreHandlerBase
+
+
+class _Test_StoreHandler(TestCase):
+    """
+    Tests for the StoreHandler class.
+    """
+
+    cls = None
+    # (method_name, nargs)
+    expected_methods = (
+        ('_get_connection', 0),
+        ('_save', 1),
+        ('_get', 1),
+        ('_delete', 1),
+        ('_list', 1),
+    )
+
+    def before(self):
+        """
+        Sets up a fresh instance of the class before each run.
+        """
+        self.instance = self.cls({})
+
+
+class Test_StoreHandlerBaseClass(_Test_StoreHandler):
+    """
+    Tests for the StoreHandlerBase class.
+    """
+
+    cls = StoreHandlerBase
+
+    def test_store_handler_base_methods(self):
+        """
+        Verify StoreHandlerBase base methods all raises.
+        """
+        for meth, nargs in self.expected_methods:
+            self.assertRaises(
+                NotImplementedError,
+                getattr(self.instance, meth),
+                *tuple(range(nargs)))
